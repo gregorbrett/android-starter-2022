c3a6e44378893f75320f340376145424dfd0a08c
gregorbrett <72610699+gregorbrett@users.noreply.github.com>
2022-09-05 13:56:08 +0100

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,74 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.kotlinKapt)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.ktLint)
+}
+
+android {
+    compileSdk = 32
+
+    defaultConfig {
+        applicationId = "eu.krzdabrowski.starter"
+        minSdk = 24
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    packagingOptions {
+        resources.excludes.add("/META-INF/{AL2.0,LGPL2.1}")
+    }
+}
+
+dependencies {
+    implementation(project(":core"))
+    implementation(project(":basic-feature"))
+
+    implementation(libs.bundles.common)
+    implementation(libs.material)
+    implementation(libs.roomKtx)
+
+    kapt(libs.hiltCompiler)
+    ksp(libs.roomCompiler)
+    coreLibraryDesugaring(libs.desugar)
+}
+
+ksp {
+    arg("room.schemaLocation", "$projectDir/schemas")
+}
